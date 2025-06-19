1c2c2a0b84bbf05ac7bea23297da4eaf23faeb3d
wnsduq4258 <wnsduq4258@naver.com>
2025-06-19 16:12:30 +0900

    refactor: controller 및 url 재정의

--- src/main/java/com/ssafy/ai/controller/AiController.java
+++ src/main/java/com/ssafy/ai/controller/AiController.java
@@ -1,5 +1,11 @@
-package com.ssafy.chatGPT.controller;
+package com.ssafy.ai.controller;
 
+import com.ssafy.client.kakao.CategoryGroupCode;
+import com.ssafy.client.kakao.KakaoMapService;
+import com.ssafy.client.kakao.dto.KakaoSearchResponse;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -9,13 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.chatGPT.dto.UserMessage;
-import com.ssafy.chatGPT.service.ChatService;
-import com.ssafy.chatGPT.service.RecommendationService;
+import com.ssafy.ai.dto.UserMessage;
+import com.ssafy.ai.service.ChatService;
+import com.ssafy.ai.service.RecommendationService;
 import com.ssafy.common.security.dto.CustomUserDetails;
 import com.ssafy.member.dto.Member;
 import com.ssafy.myplace.dto.RecommendPlaceDto;
-import static com.ssafy.chatGPT.constant.RecommendationConstants.*;
+import static com.ssafy.ai.constant.RecommendationConstants.*;
 
 import java.util.List;
 
@@ -24,10 +30,11 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/ai")
 @RequiredArgsConstructor
-public class ChatController {
+public class AiController {
 
     private final ChatService chatService;
     private final RecommendationService recommendationService;
+    private final KakaoMapService kakaoMapService;
 
     @PostMapping("/chat")
     public ResponseEntity<String> chat(@RequestBody UserMessage userMessage, @AuthenticationPrincipal CustomUserDetails userDetails) {
@@ -36,14 +43,14 @@ public class ChatController {
         return ResponseEntity.ok(reply);
     }
     
-    @GetMapping("/recommend/places/mbti")
+    @GetMapping("/recommendation/places/mbti")
     public ResponseEntity<List<RecommendPlaceDto>> getRecommendPlacesByMBTI(@AuthenticationPrincipal CustomUserDetails userDetails) {
     	Member loginUser = userDetails.getMember();
     	List<RecommendPlaceDto> body = recommendationService.recommendPlacesByMbti(loginUser, DEFAULT_RECOMMEND_LIMIT);
     	return ResponseEntity.ok(body);
     }
     
-    @GetMapping("/recommend/places/characters") 
+    @GetMapping("/recommendation/places/characters")
     public ResponseEntity<List<RecommendPlaceDto>> getRecommendPlacesByCharacters(@AuthenticationPrincipal CustomUserDetails userDetails) {
     	Member loginUser = userDetails.getMember();
     	List<RecommendPlaceDto> body = recommendationService.recommendByCharacters(loginUser, DEFAULT_RECOMMEND_LIMIT);
