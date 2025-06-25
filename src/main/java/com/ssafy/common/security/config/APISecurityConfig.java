422b4b41ef7ec47d588fdf6ccc6bdf2d422dfc0a
Kangboom <jkl6147@naver.com>
2025-06-25 23:05:02 +0900

    style: 코드 리포맷팅

--- src/main/java/com/ssafy/common/security/config/APISecurityConfig.java
+++ src/main/java/com/ssafy/common/security/config/APISecurityConfig.java
@@ -1,7 +1,10 @@
 package com.ssafy.common.security.config;
 
+import com.ssafy.common.security.filter.JWTAuthenticationFilter;
+import com.ssafy.common.security.filter.JWTVerificationFilter;
+import com.ssafy.common.security.filter.SecurityExceptionHandlingFilter;
+import com.ssafy.common.security.service.CustomUserDetailsService;
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,53 +20,42 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.ssafy.common.security.filter.JWTAuthenticationFilter;
-import com.ssafy.common.security.filter.JWTVerificationFilter;
-import com.ssafy.common.security.filter.SecurityExceptionHandlingFilter;
-import com.ssafy.common.security.service.CustomUserDetailsService;
-
 @Configuration
 public class APISecurityConfig {
-
+    
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-
-	@Bean
-	PasswordEncoder passwordEncoder() {
-//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		return new BCryptPasswordEncoder();
-	}
-	
+    
     @Bean
-    public SecurityFilterChain apiSecurityFilterChain(
-            HttpSecurity http,
-            @Qualifier("corsConfigurationSource") CorsConfigurationSource corsConfig,
-            CustomUserDetailsService userDetailsService,
-            JWTAuthenticationFilter authFilter,
-            JWTVerificationFilter jwtVerifyFilter,
-            SecurityExceptionHandlingFilter exceptionFilter)
-            throws Exception {
-    	
-    	http.securityMatcher("/api/**")
-    	.cors(t -> t.configurationSource(corsConfig))
-    	.userDetailsService(userDetailsService)
-    	.csrf(csrf -> csrf.disable())
-    	.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    	
-    	http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").permitAll()
-    			.anyRequest().authenticated());
-    	
-    	http.addFilterBefore(jwtVerifyFilter, UsernamePasswordAuthenticationFilter.class)
-    	.addFilterAt(authFilter, UsernamePasswordAuthenticationFilter.class)
-    	.addFilterBefore(exceptionFilter, JWTVerificationFilter.class);
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http, @Qualifier("corsConfigurationSource") CorsConfigurationSource corsConfig, CustomUserDetailsService userDetailsService,
+            JWTAuthenticationFilter authFilter, JWTVerificationFilter jwtVerifyFilter, SecurityExceptionHandlingFilter exceptionFilter) throws Exception {
         
-    	// END
+        http.securityMatcher("/api/**")
+                .cors(t -> t.configurationSource(corsConfig))
+                .userDetailsService(userDetailsService)
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+        
+        // SecurityFilter 앞에 API 로그 필터를 추가
+        http.addFilterBefore(jwtVerifyFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionFilter, JWTVerificationFilter.class);
+        
         return http.build();
     }
-
-    // 06-2. Filter 수준에서 동작하기 위한 CorsConfigurationSource를 구성하고 적용하자.
+    
     // WebMvcConfigurer는 cors 설정은 무의미
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -71,15 +63,14 @@ public class APISecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-
+        
         //“클라이언트(브라우저)가 쿠키, 인증 헤더(JWT 등), 세션 같은 자격 정보(credentials)를 서버로 보낼 수 있게 허용하겠다”
         configuration.setAllowCredentials(true);
         
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", configuration);
         source.registerCorsConfiguration("/member/checkEmail", configuration);
-
+        
         return source;
     }
-    
 }
