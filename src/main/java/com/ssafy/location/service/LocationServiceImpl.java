4e20d2e13abe25d4c895c53b639a6d7c975f7174
ahyolo27 <ahhyeon27@gmail.com>
2025-06-26 22:48:20 +0900

    feature: @Transactional 어노테이션 추가

--- src/main/java/com/ssafy/location/service/LocationServiceImpl.java
+++ src/main/java/com/ssafy/location/service/LocationServiceImpl.java
@@ -7,6 +7,7 @@ import com.ssafy.location.mapper.SidoMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class LocationServiceImpl implements LocationService {
   private final SidoMapper sidoMapper;
   private final GugunMapper gugunMapper;
 
+  @Transactional
   @Override
   public void insertAllSidos(List<Sido> list) {
     sidoMapper.insertAll(list);
@@ -25,6 +27,7 @@ public class LocationServiceImpl implements LocationService {
     return sidoMapper.getAll();
   }
 
+  @Transactional
   @Override
   public void insertAllGuguns(List<Gugun> list) {
     gugunMapper.insertAll(list);
