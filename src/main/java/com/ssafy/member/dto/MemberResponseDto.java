0c68cbf4f9e2e5bfd52ebb5f8e717b358fbff611
Kangboom <jkl6147@naver.com>
2025-06-30 15:30:08 +0900

    feature: 조회된 멤버를 담을 응답 dto 생성

---
+++ src/main/java/com/ssafy/member/dto/MemberResponseDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponseDto {
+    
+    private Long id;
+    private String email;
+    private String name;
+    private Role role;
+}
