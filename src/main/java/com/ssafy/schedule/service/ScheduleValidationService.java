818ac433725937fcfade791ec5376e44f8430a41
ahyolo27 <ahhyeon27@gmail.com>
2025-07-01 15:15:05 +0900

    feature: 스케줄 추가 로직 간소화

--- src/main/java/com/ssafy/schedule/service/ScheduleValidationService.java
+++ src/main/java/com/ssafy/schedule/service/ScheduleValidationService.java
@@ -21,15 +21,6 @@ public class ScheduleValidationService {
     LocalDate startDate = planner.getStartDay();
     LocalDate endDate = planner.getEndDay();
 
-    if (schedules.size() == 1) { // 단일 스케줄인 경우 날짜만 확인
-      LocalDate thisDate = schedules.get(0).date();
-      if (planner.getStartDay().isBefore(thisDate)
-          || planner.getEndDay().isAfter(thisDate)) {
-        throw new IllegalArgumentException("스케줄이 플래너 기간을 벗어났습니다.");
-      }
-      return;
-    }
-
     // 1) 전체 기간을 벗어났는지 확인
     for (ScheduleDto schedule : schedules) {
       if (schedule.date().isBefore(startDate)
@@ -38,18 +29,20 @@ public class ScheduleValidationService {
       }
     }
 
-    // 2) 인접 스케줄 간 날짜, 순서 시간 순서 확인
-    for (int i = 0; i < schedules.size() - 1; i++) {
-      ScheduleDto cur = schedules.get(i);
-      ScheduleDto next = schedules.get(i + 1);
+    if (schedules.size() > 1) {
+      // 2) 인접 스케줄 간 날짜, 순서 시간 순서 확인
+      for (int i = 0; i < schedules.size() - 1; i++) {
+        ScheduleDto cur = schedules.get(i);
+        ScheduleDto next = schedules.get(i + 1);
 
-      LocalDateTime curTime = LocalDateTime.of(cur.date(), cur.startTime());
-      LocalDateTime nextTime = LocalDateTime.of(next.date(), next.startTime());
+        LocalDateTime curTime = LocalDateTime.of(cur.date(), cur.startTime());
+        LocalDateTime nextTime = LocalDateTime.of(next.date(), next.startTime());
 
-      if (curTime.isAfter(nextTime)) {
-        throw new IllegalArgumentException(
-            String.format("일정 검증 실패: idx %d 와 %d 사이의 날짜/시간 순서가 올바르지 않습니다.", i, i + 1)
-        );
+        if (curTime.isAfter(nextTime)) {
+          throw new IllegalArgumentException(
+              String.format("일정 검증 실패: idx %d 와 %d 사이의 날짜/시간 순서가 올바르지 않습니다.", i, i + 1)
+          );
+        }
       }
     }
   }
