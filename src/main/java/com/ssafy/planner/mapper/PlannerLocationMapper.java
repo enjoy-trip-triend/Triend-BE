70031b95b4c96269602b89dc000aad72677861bb
ahyolo27 <ahhyeon27@gmail.com>
2025-07-02 18:17:40 +0900

    feature: plannerLocation 매퍼에 delete 추가

--- src/main/java/com/ssafy/planner/mapper/PlannerLocationMapper.java
+++ src/main/java/com/ssafy/planner/mapper/PlannerLocationMapper.java
@@ -10,4 +10,5 @@ public interface PlannerLocationMapper {
 
   int insertLocation(@Param("plannerId") Long plannerId, @Param("list") List<PlannerLocationDto> list);
 
+  void deleteLocation(@Param("plannerId") Long PlannerId);
 }
