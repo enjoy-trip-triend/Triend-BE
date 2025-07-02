94a74d213b67c8f6352d5699eded54832bd5afdc
Kangboom <jkl6147@naver.com>
2025-07-02 18:02:27 +0900

    feature: 스케줄 응답 dto 생성

---
+++ src/main/java/com/ssafy/schedule/dto/ScheduleResponseDto.java
@@ -0,0 +1,22 @@
+package com.ssafy.schedule.dto;
+
+import com.ssafy.place.dto.PlaceResponseDto;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleResponseDto {
+    
+    private Long id;
+    private Long plannerId;
+    private LocalDate date;
+    private LocalTime startTime;
+    private String content;
+    private Integer idx;
+    private PlaceResponseDto place;
+}
