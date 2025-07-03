cc62a677935ad7672b37f30ea3c81d9fe3e6c0bb
wnsduq4258 <wnsduq4258@naver.com>
2025-07-03 22:02:22 +0900

    feature: 사용자 맞춤형 장소 추천 AttractionRecommendationService 기능

---
+++ src/main/java/com/ssafy/ai/service/AttractionRecommendationService.java
@@ -0,0 +1,53 @@
+package com.ssafy.ai.service;
+
+import static com.ssafy.client.kakao.constant.KakaoApiConstants.*;
+
+import com.ssafy.client.kakao.CategoryGroupCode;
+import com.ssafy.client.kakao.KakaoMapService;
+import com.ssafy.client.kakao.dto.KakaoSearchResponseDto.Document;
+import com.ssafy.member.dto.Member;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AttractionRecommendationService {
+
+    private final KakaoMapService kakaoMapService;
+    private final AiService aiService;
+
+    /**
+     * 사용자에게 맞춤형 관광지를 추천해줍니다.
+     *
+     * @param member  현재 로그인한 멤버
+     * @param regions 사용자가 플래너에 담은 지역
+     * @return 사용자 맞춤 관광지 장소 반환
+     */
+    public List<Document> recommendAttraction(Member member,
+            List<String> regions) {
+
+        if (regions == null || regions.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Document> documents = regions.parallelStream()
+                .flatMap(region -> kakaoMapService.searchPlacesByKeyword(region,
+                                CategoryGroupCode.AT4, DEFAULT_PAGE, DEFAULT_SIZE)
+                        .stream())
+                .distinct()
+                .toList();
+
+        log.debug("documents: {}", documents);
+
+        if (documents.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return aiService.filterAttractionsByAi(member, documents);
+    }
+
+}
