ef251d7ad1555dbbb39d3f976f981eec538e56a3
wnsduq4258 <wnsduq4258@naver.com>
2025-07-04 21:40:20 +0900

    refactor: 카카오 API 상수 수정

--- src/main/java/com/ssafy/client/kakao/constant/KakaoApiConstants.java
+++ src/main/java/com/ssafy/client/kakao/constant/KakaoApiConstants.java
@@ -5,6 +5,5 @@ public final class KakaoApiConstants {
 
     public static final String KAKAO_AUTH_HEADER = "KakaoAK ";
     public static final int DEFAULT_PAGE = 1;
-    public static final int DEFAULT_SIZE = 20;
-    public static final int MAX_SIZE = 30;
+    public static final int DEFAULT_SIZE = 15;  // 카카오 MAP API size 최대 15까지 가능
 }
