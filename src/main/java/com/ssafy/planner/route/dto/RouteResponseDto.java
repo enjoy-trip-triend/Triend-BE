8586d0ad16bdba4a5b31e72f91062503e02896b5
Kangboom <jkl6147@naver.com>
2025-07-07 23:45:51 +0900

    feature: 경로추천 응답 dto 작성

---
+++ src/main/java/com/ssafy/planner/route/dto/RouteResponseDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.planner.route.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RouteResponseDto {
+    
+    public List<String> route;
+    public double totalDistance;
+}
