fd88bd93f3f58454d8ed3ea3110fd9ad92ae200f
Kangboom <jkl6147@naver.com>
2025-07-13 13:16:24 +0900

    chore: rebase 충돌 해결

---
+++ src/main/java/com/ssafy/schedule/service/ScheduleService.java
@@ -0,0 +1,23 @@
+package com.ssafy.schedule.service;
+
+import com.ssafy.common.security.dto.CustomUserDetails;
+import com.ssafy.schedule.dto.ScheduleCreateRequestDto;
+import com.ssafy.schedule.dto.SchedulesOrderUpdateRequestDto;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public interface ScheduleService {
+    
+    void createSchedules(ScheduleCreateRequestDto request, CustomUserDetails loginUser);
+    
+    void deleteSchedule(Long planId, CustomUserDetails loginUser);
+    
+    void deleteSchedulesByPlannerAndDate(Long plannerId, LocalDate startDay, LocalDate endDay);
+    
+    void deleteSchedulesByPlanner(Long plannerId);
+    
+    Map<Long, List<String>> getPresignedImageUrlsByScheduleIds(List<Long> planIds);
+    
+    void updateSchedulesOrder(SchedulesOrderUpdateRequestDto request, CustomUserDetails loginUser);
+}
